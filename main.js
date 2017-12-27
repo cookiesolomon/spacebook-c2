6734844cc5ec393fea5f7c5c29e894e20421bcaf
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-27 17:56:31 +0200

    updates

--- main.js
+++ main.js
@@ -12,26 +12,36 @@ $('button').on('click', function () {
 
 });
 
+//remove btn click event
+$('.posts').on('click', '.remove-post',function () {
+    var dataId = $(this).closest('p').data().id;
+    //remove from the array
+    posts.splice(dataId, 1);
+    //rerender the posts
+    renderPosts();
+});
+
+
 function renderPosts() {
+    
     //removes all posts
-    $('.posts').find('p').remove();
+    $('.posts').empty();
     //reappends all posts
     for (i = 0; i < posts.length; i++) {
-        $('.posts').append('<p data-id="' + posts[i].id + '">' + ' Name: ' + posts[i].text + 'ID: ' + posts[i].id + '  <button type="button" class="remove">Remove Post</button><button type="button" class="see-posts">See Comments</button></p>');
-    }
-
-    //remove btn click event
-    $('.remove').on('click', function () {
-        //remove from the array
-        for (var j = 0; j < posts.length; j++) {
-            if ($('p').data().id === posts[j].id) {
-                posts.splice(posts[j], 1);
-            }
-        }
-        //rerender the posts
-        renderPosts()
-    });
+        $('.posts').append('<div class="post">' +
+        '<p data-id="' + posts[i].id + '">' + ' Name: ' + posts[i].text + 'ID: ' + posts[i].id + 
+        '<button type="button" class="remove-post">Remove Post</button>'+
+        '</p>' +
+        '<input id="user-name" type="text" placeholder="username"><br><input id="comments-area" type="text" placeholder="leave a comment...">'+
+        '<button type="button" class="comments-button">Comment!</button><button type="button" class="see-posts">See Comments</button>' +
+        '<div class="comments"></div>'+
+        '</div>'
+    );
 }
+
+}
+
+// function for adding comments to div class = comments above 
 
 // //INDIVIDUAL PROJECT EXTENSION 1 (OPTIONAL!)
 // Add a feature that allows each post to receive a comment. Each post will require it's own form,
@@ -40,7 +50,7 @@ function renderPosts() {
 
 function comments() {
     $('.posts').append(
-        '<form><input id="user-name" type="text" placeholder="username"><br><input id="comments-area" type="text" placeholder="leave a comment..."><br><button type="button" class="comments-button">Click Me!</button><button type="button" class="delete">Delete</button></form>')
+       )
 
 
     $('.comments-button').on('click', function () {
